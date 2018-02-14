5192ec875ba6cd9f956966937238e1269960574d
zhaobc <zhbchwin@163.com>
2018-02-15 00:43:58 +0800

    发布一版示例。情人节&腊月30，熬夜改bug也是没谁了。。。

--- src/config/index.js
+++ src/config/index.js
@@ -2,8 +2,8 @@
 const ip = '192.168.1.86';
 
 // export const DOMAIN = `zhaobc.coding.me/weex`;
-// export const DOMAIN = `fuckdoctors.github.io/hello-weex`;
-export const DOMAIN = `${ip}:8081`;
+export const DOMAIN = `fuckdoctors.github.io/hello-weex`;
+// export const DOMAIN = `${ip}:8081`;
 export const DIST = 'dist';
 
 export const APP_NAME = 'WeexDemo';
