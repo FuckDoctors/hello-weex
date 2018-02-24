57399027b58a14e2f084a872519a0115cacb8601
FuckDoctors <zhbchwin@163.com>
2018-02-24 16:47:50 +0800

    webpack.docs.conf.js实现代码分割，并发布一版Demo。

--- configs/webpack.docs.conf.js
+++ configs/webpack.docs.conf.js
@@ -37,7 +37,8 @@ const generateMultipleEntrys = (entry) => {
         isDevServer: true,
         chunksSortMode: 'dependency',
         inject: true,
-        chunks: [name],
+        // chunks: [name],
+        chunks: ['vendor', 'manifest', 'common', name],
         // production
         minimize: true
       });
@@ -189,7 +190,33 @@ const docsConfig = webpackMerge(commonConfig[0], {
         drop_console: true,
         drop_debugger: true
       }
-    })
+    }),
+
+    // split vendor js into its own file
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+      minChunks: function (module, count) {
+        // any required modules inside node_modules are extracted to vendor
+        return (
+          module.resource &&
+          /\.js$/.test(module.resource) &&
+          module.resource.indexOf(
+            path.join(__dirname, '../node_modules')
+          ) === 0
+        )
+      }
+    }),
+    // extract webpack runtime and module manifest to its own file in order to
+    // prevent vendor hash from being updated whenever app bundle is updated
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'manifest',
+      chunks: ['vendor']
+    }),
+
+    new webpack.optimize.CommonsChunkPlugin({
+      async: 'common',
+      minChunks: (module, count) => count >= 2
+    }),
   ]
 });
 
