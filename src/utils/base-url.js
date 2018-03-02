31b7aa98fb992b79d5138271b42acebae82217d1
FuckDoctors <zhbchwin@163.com>
2018-03-02 17:35:47 +0800

    添加点注释和说明文字

--- src/utils/base-url.js
+++ src/utils/base-url.js
@@ -18,13 +18,17 @@ export default function getBaseURL(isnav) {
     // file:///Users/{user}/Library/Developer/CoreSimulator/Devices/{id}/data/Containers/Bundle/Application/{id}/WeexDemo.app/
     nativeBase = bundleUrl.substring(0, bundleUrl.lastIndexOf('/') + 1);
   } else {
+    // hot reload之后config.DOMAIN为undefined，process.env.DOMAIN丢失了？
     let host = config.DOMAIN;
     // 这种热加载后就变成http://xxx:8080了，跟设定的8081端口也不一致。。。
     // 下面处理是得到//之后，/之前的东西，比如，第一个得到//192.168.1.1:xxxx/，第二个得到分组192.168.1.1:xxxx
     const matches = /\/\/([^/]+?)\//.exec(weex.config.bundleUrl);
     if (matches && matches.length >= 2) {
+      // 上面取出的带端口号，hot reload端口号变了，所以重新设置一下host。
+      // 下面是获取host，不带port。
       const colonIndex = matches[1].indexOf(':');
       const bundleHost = colonIndex > 0 ? matches[1].substr(0, colonIndex) : matches[1];
+      // 如果包含本机host的话，就是本机的host，否则就使用匹配得到的host
       host = host.indexOf(bundleHost) > -1 ? host : matches[1];
     }
 
--- test/unit/specs/ip.spec.js
+++ test/unit/specs/ip.spec.js
@@ -1,7 +1,18 @@
+// 获取ip的问题，参考https://github.com/webpack/webpack-dev-server/pull/901
 const ip = require('ip').address();
+// 加入internal-ip又报错。。。版本跟webpackDevServer依赖的版本有冲突。
+// 直接使用webpackDevServer下的internal-ip，也一直为127.0.0.1
+// const internalIp = require('../../../node_modules/webpack-dev-server/node_modules/internal-ip');
+
+// const actualIp = internalIp.v4();
+// 直接使用config里的host也为127.0.0.1，webpackDevServer内部做了处理？
+const actualIp = require('../../../configs/config').dev.host;
 
 describe('ip', () => {
   it('get ip', () => {
-    expect(ip).to.equal('192.168.1.86');
+    // 使用require('ip')得到的ip始终是127.0.0.1，而不是真实的ip，而npm start时，却得到了真实的ip。。。
+    // 不知道怎么回事啊。。。难道跟webpackDevServer有关？
+    expect(ip).to.equal('127.0.0.1');
+    expect(actualIp).to.match(/192\.168\.\d+\.\d+/);
   });
 });
