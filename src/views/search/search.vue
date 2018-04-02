0d38e47bb717ffbf4c1896d892d33d312fa68d52
FuckDoctors <zhbchwin@163.com>
2018-04-02 16:57:51 +0800

    设置最大搜索历史为10个，添加清空搜索历史功能。

--- src/views/search/search.vue
+++ src/views/search/search.vue
@@ -13,13 +13,27 @@
                      @searchbarInputOnFocus="searchbarInputOnFocus"></search-bar>
     </div>
 
-    <div class="history-list">
-      <text lines="1" class="history-key"
-        @click="clickHistory(item)"
-        v-for="(item, index) in history" :key="index">{{item.key}}</text>
-    </div>
-
     <scroller class="scroller">
+      <div class="history-label-wrapper">
+        <text class="history-label">搜索历史</text>
+        <wxc-icon class="history-clear" name="delete"
+          :iconStyle="{fontSize: '28px'}"
+          @wxcIconClicked="clearHistory()" />
+      </div>
+
+      <div class="history-list">
+        <template v-if="history.length > 0">
+          <text lines="1" class="history-key"
+            @click="clickHistory(item)"
+            v-for="(item, index) in history" :key="index">{{item.key}}</text>
+        </template>
+        <template v-else>
+          <div class="history-empty">
+            <text class="history-empty-tip">空空如也，快搜索试试吧~</text>
+          </div>
+        </template>
+      </div>
+
       <div class="instant-result">
         <div class="instant-shop">
           <div class="shop-item">
@@ -30,11 +44,17 @@
         </div>
         <div class="instant-keywords"></div>
       </div>
+
+      <div class="result-list">
+        <div class="result-item"></div>
+      </div>
     </scroller>
   </div>
 </template>
 
 <script>
+import { WxcIcon } from 'weex-ui';
+
 import NavBar from '@/components/nav-bar';
 import SearchBar from '@/components/search-bar';
 import helper from '@/utils/helper';
@@ -45,12 +65,14 @@ export default {
   components: {
     NavBar,
     SearchBar,
+    WxcIcon,
   },
   data() {
     return {
       searchKey: '',
       history: [],
       result: [],
+      instantResult: [],
     };
   },
   mounted() {
@@ -59,18 +81,16 @@ export default {
       '奶粉',
       '超级能恩',
       '超级能恩3段',
-      '一',
       '雀巢超级能恩3段',
       '纸尿裤',
       '花王纸尿裤',
       '超长内容超长内容超长内容超长内容超长内容超长内容超长内容超长内容超长内容',
       '儿童车',
-      '1',
-    ].map((value) => {
+    ].map((value, index) => {
       const obj = {
         weight: 1,
         key: value,
-        date,
+        date: date - index,
       };
       return obj;
     });
@@ -96,6 +116,17 @@ export default {
       }
       // 排序
       this.history.sort((a, b) => b.date - a.date);
+      // 只保留最近10条
+      if (this.history.length > 10) {
+        // 下面这句，会返回删掉的数组，而原数组本身只会保留删除后的内容。
+        // 所以这种情况，splice后，history只有一个了。
+        // this.history.splice(0, 10);
+        this.history.splice(10, this.history.length - 10);
+      }
+    },
+    clearHistory() {
+      console.log('clear history...');
+      this.history = [];
     },
     searchbarInputReturned() {
       if (this.searchKey.length === 0) {
@@ -145,6 +176,29 @@ export default {
   color: #ffffff;
 }
 
+.scroller {
+  flex: 1;
+}
+
+.history-label-wrapper {
+  font-size: 36px;
+  font-weight: bold;
+  padding-top: 10px;
+  padding-left: 15px;
+  padding-right: 15px;
+  flex-direction: row;
+  align-items: center;
+}
+.history-label {
+  flex: 1;
+  color: #666666;
+}
+.history-clear {
+  /* width: 60px; */
+  /* justify-content: center; */
+  /* cursor: pointer; */
+}
+
 .history-list {
   /* margin-top: 40px; */
   margin-bottom: 40px;
@@ -171,5 +225,16 @@ export default {
   line-height: 38px;
   max-width: 660px;
   text-overflow: ellipsis;
+}
+.history-empty {
+  flex: 1;
+  padding-top: 15px;
+  padding-right: 15px;
+  padding-left: 15px;
+  justify-content: center;
+  align-items: center;
+}
+.history-empty-tip {
+  color: #666666;
 }
 </style>
