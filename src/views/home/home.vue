dfe3d606b3f8871b973b027e6eb3ebff7a7305c7
zhaobc <zhbchwin@163.com>
2018-04-10 00:08:09 +0800

    加入eleme主页上的分组

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -17,9 +17,25 @@
     <scroller class="scroller">
       <div class="banner">
         <wx-slider :slider-style="{width: '750px', height: '320px'}"
-                :auto-play="true">
+              :auto-play="true">
           <div class="slider-item" v-for="(item, index) in bannerSliders" :key="index">
             <image class="banner-slider-image" :src="item.img" />
+          </div>
+        </wx-slider>
+      </div>
+
+      <div class="shortcuts-container">
+        <wx-slider :auto-play="false"
+                :indicator-style="{itemSize: '10px', itemSelectedColor: '#000000'}">
+          <div class="slider-item">
+            <div class="shortcut-page"
+                  v-for="n in pageRange(shortcuts.length, shortcutsPageSize)" :key="n">
+              <div class="shortcut-wrapper"
+                  v-for="i in pageRecordsRange(shortcuts.length, shortcutsPageSize, n)" :key="i">
+                <image :src="shortcuts[i].icon" class="shortcut-icon" />
+                <text class="shortcut-title">{{shortcuts[i].title}}</text>
+              </div>
+            </div>
           </div>
         </wx-slider>
       </div>
@@ -63,10 +79,16 @@ export default {
       location: '',
       geo: {},
       bannerSliders: [],
+      shortcuts: [],
+      shortcutsPageSize: 10,
     };
   },
-  mounted() {
+  created() {
+    // 放在mounted里shortcuts取不到东西，所以挪到created里。
     this.init();
+  },
+  mounted() {
+    // this.init();
   },
   methods: {
     init() {
@@ -85,6 +107,9 @@ export default {
 
       // banner
       this.initBannerSlider();
+
+      // shortcuts
+      this.initShortcuts();
     },
     initBannerSlider() {
       this.bannerSliders = [
@@ -108,6 +133,81 @@ export default {
           img: 'https://m.360buyimg.com/mobilecms/s828x404_jfs/t18841/5/1285260207/98918/82c8b3be/5ac3459dN292e8395.jpg!cr_1125x549_0_72!q70.jpg',
         },
       ];
+    },
+    initShortcuts() {
+      /* eslint-disable */
+      this.shortcuts = [
+        {
+          title: '美食',
+          icon: '//fuss10.elemecdn.com/7/d8/a867c870b22bc74c87c348b75528djpeg.jpeg?imageMogr/format/webp/thumbnail/!90x90r/gravity/Center/crop/90x90/',
+        },
+        {
+          title: '商超便利',
+          icon: '//fuss10.elemecdn.com/c/3c/0184f5b3fa72f295fc01864734218jpeg.jpeg?imageMogr/format/webp/thumbnail/!90x90r/gravity/Center/crop/90x90/',
+        },
+        {
+          title: '夜宵',
+          icon: '//fuss10.elemecdn.com/9/21/60ac33f023d9074e13cd78f9b5964jpeg.jpeg?imageMogr/format/webp/thumbnail/!90x90r/gravity/Center/crop/90x90/',
+        },
+        {
+          title: '果蔬生鲜',
+          icon: '//fuss10.elemecdn.com/c/db/d20d49e5029281b9b73db1c5ec6f9jpeg.jpeg?imageMogr/format/webp/thumbnail/!90x90r/gravity/Center/crop/90x90/',
+        },
+        {
+          title: '医药健康',
+          icon: '//fuss10.elemecdn.com/7/0a/af108e256ebc9f02db599592ae655jpeg.jpeg?imageMogr/format/webp/thumbnail/!90x90r/gravity/Center/crop/90x90/',
+        },
+        {
+          title: '大牌5折',
+          icon: '//fuss10.elemecdn.com/a/7b/b02bd836411c016935d258b300cfejpeg.jpeg?imageMogr/format/webp/thumbnail/!90x90r/gravity/Center/crop/90x90/',
+        },
+        {
+          title: '浪漫鲜花',
+          icon: '//fuss10.elemecdn.com/3/01/c888acb2c8ba9e0c813f36ec9e90ajpeg.jpeg?imageMogr/format/webp/thumbnail/!90x90r/gravity/Center/crop/90x90/',
+        },
+        {
+          title: '麻辣烫',
+          icon: '//fuss10.elemecdn.com/e/c7/b7ba9547aa700bd20d0420e1794a8jpeg.jpeg?imageMogr/format/webp/thumbnail/!90x90r/gravity/Center/crop/90x90/',
+        },
+        {
+          title: '地方菜系',
+          icon: '//fuss10.elemecdn.com/a/8a/ec21096d528b7cfd23cdd894f01c6jpeg.jpeg?imageMogr/format/webp/thumbnail/!90x90r/gravity/Center/crop/90x90/',
+        },
+        // {
+        //   title: '披萨意面',
+        //   icon: '//fuss10.elemecdn.com/7/b6/235761e50d391445f021922b71789jpeg.jpeg?imageMogr/format/webp/thumbnail/!90x90r/gravity/Center/crop/90x90/',
+        // },
+        // {
+        //   title: '地方小吃',
+        //   icon: '//fuss10.elemecdn.com/7/d6/6f2631288a44ec177204e05cbcb93jpeg.jpeg?imageMogr/format/webp/thumbnail/!90x90r/gravity/Center/crop/90x90/',
+        // },
+
+        /* eslint-enable */
+      ];
+    },
+    pageRange(totalRecords, pageSize) {
+      // 想实现v-for range效果
+      // https://github.com/vuejs/vue/issues/3641
+      // 分页计算公式：总页数=（总记录数+每页显示数-1）/每页显示数
+      // 或者下面的形式
+      // 更多参考https://blog.csdn.net/u014424628/article/details/50553002
+      let totalPages = ((totalRecords - 1) / pageSize) + 1;
+      // js计算后得到小数，所以做了一些转换。
+      totalPages = parseInt(totalPages, 10);
+      return Array(totalPages).fill(0).map((value, index) => index + 1);
+    },
+    pageRecordsRange(totalRecords, pageSize, page) {
+      // 更多分页相关计算参考这个：https://blog.csdn.net/imhxl/article/details/52774996
+      const pageStart = (page - 1) * pageSize;
+      let pageEnd = pageStart + pageSize;
+      if (pageEnd > totalRecords) {
+        pageEnd = pageStart + (totalRecords % pageSize);
+      }
+
+      // 返回当前页的范围
+      const arr = Array(pageEnd - pageStart).fill(0).map((value, index) => pageStart + index);
+      console.log(arr);
+      return arr;
     },
     changeLocation() {
       modal.toast({
@@ -170,4 +270,27 @@ export default {
   height: 320px;
 }
 
+.shortcuts-container {
+  background-color: #ffffff;
+}
+.shortcut-page {
+  flex: 1;
+  flex-direction: row;
+  flex-wrap: wrap;
+}
+.shortcut-wrapper {
+  flex-basis: 145px;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+.shortcut-icon {
+  width: 90px;
+  height: 90px;
+}
+.shortcut-title {
+  font-size: 24px;
+  color: #666666;
+  margin-top: 10px;
+}
 </style>
